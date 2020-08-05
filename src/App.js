17c1141d3197a35c892912cba8e3e4b6eb7cf9af
Sameen Shakeel <sameenshakeel1997@gmail.com>
2020-08-05 12:30:20 -0700

    Implemented Animations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import React from 'react';
 import './App.css';
+import useWebAnimations from "@wellyshen/use-web-animations";
 
 function App() {
+  const { ref: alice } = useWebAnimations({
+    keyframes: [
+      { transform: 'translateY(0)' },
+      { transform: 'translateY(-100%)' }
+    ],
+    timing: {
+      easing: 'steps(7, end)',
+      direction: "reverse",
+      duration: 600,
+      playbackRate: 1,
+      iterations: Infinity
+    },
+    // onReady: ({ playState, animate, animation }) => {
+
+    // },
+    // onUpdate: ({ playState, animate, animation }) => {
+
+    // },
+    // onFinish: ({ playState, animate, animation }) => {
+
+    // },
+  });
+
   return (
-    <div>
-      <h1>Helllooo!</h1>
+    <div className="wrapper">
+      <div className="sky"></div>
+      <div className="earth">
+        <div id="red-queen_and_alice">
+          <img id="red-queen_and_alice_sprite" ref={alice} src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png" alt="Alice and the Red Queen running to stay in place." />
+        </div>
+      </div>
+      <div class="scenery" id="foreground1">
+        <img id="palm3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png" alt=" " />
+      </div>
+      <div class="scenery" id="foreground2">
+        <img id="bush" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png" alt=" " />
+        <img id="w_rook_upright" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png" alt=" " />
+      </div>
+      <div class="scenery" id="background1">
+        <img id="r_pawn_upright" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" alt=" " />
+        <img id="w_rook" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png" alt=" " />
+        <img id="palm1" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png" alt=" " />
+      </div>
+      <div class="scenery" id="background2">
+        <img id="r_pawn" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png" alt=" " />
+
+        <img id="r_knight" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png" alt=" " />
+        <img id="palm2" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png" alt=" " />
+      </div>
     </div>
   );
 }
