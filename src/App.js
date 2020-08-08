7dc0fb632e563f3f6a561d14b9e240bbe653f27e
Sameen Shakeel <sameenshakeel1997@gmail.com>
2020-08-08 11:08:34 -0700

    implemented WAAPI

--- src/App.js
+++ src/App.js
@@ -3,54 +3,116 @@ import './App.css';
 import useWebAnimations from "@wellyshen/use-web-animations";
 
 function App() {
-  const { ref: alice } = useWebAnimations({
+  const { ref: cube, getAnimation } = useWebAnimations({
     keyframes: [
       { transform: 'translateY(0)' },
       { transform: 'translateY(-100%)' }
     ],
     timing: {
-      easing: 'steps(7, end)',
+      easing: 'steps(13, end)',
       direction: "reverse",
       duration: 600,
-      playbackRate: 1,
       iterations: Infinity
     },
-    // onReady: ({ playState, animate, animation }) => {
-
-    // },
-    // onUpdate: ({ playState, animate, animation }) => {
-
-    // },
-    // onFinish: ({ playState, animate, animation }) => {
-
-    // },
   });
+
+  const { ref: background1, getAnimation: animBackground1 } = useWebAnimations({
+    keyframes: [
+      { transform: 'translateX(100%)' },
+      { transform: 'translateX(-100%)' }
+    ],
+    timing: {
+      duration: 36000,
+      iterations: Infinity
+    },
+  });
+
+  const { ref: background2, getAnimation: animBackground2 } = useWebAnimations({
+    keyframes: [
+      { transform: 'translateX(100%)' },
+      { transform: 'translateX(-100%)' }
+    ],
+    timing: {
+      duration: 30000,
+      iterations: Infinity
+    },
+  });
+
+  const { ref: foreground1, getAnimation: animForeground1 } = useWebAnimations({
+    keyframes: [
+      { transform: 'translateX(100%)' },
+      { transform: 'translateX(-100%)' }
+    ],
+    timing: {
+      duration: 10000,
+      iterations: Infinity
+    },
+  });
+
+  const { ref: foreground2, getAnimation: animForeground2 } = useWebAnimations({
+    keyframes: [
+      { transform: 'translateX(100%)' },
+      { transform: 'translateX(-100%)' }
+    ],
+    timing: {
+      duration: 12000,
+      iterations: Infinity
+    },
+  });
+
+  const play = () => {
+    getAnimation().play();
+    animBackground1().play();
+    animBackground2().play();
+    animForeground1().play();
+    animForeground2().play();
+  };
+
+  const pause = () => {
+    getAnimation().pause();
+    animBackground1().pause();
+    animBackground2().pause();
+    animForeground1().pause();
+    animForeground2().pause();
+  }
+
+  const reverse = () => {
+    getAnimation().reverse();
+    animBackground1().reverse();
+    animBackground2().reverse();
+    animForeground1().reverse();
+    animForeground2().reverse();
+  };
 
   return (
     <div className="wrapper">
+      <div className="buttons">
+        <button id="btn" onClick={play}>Play</button>
+        <button id="btn"onClick={pause}>Pause</button>
+        <button id="btn"onClick={reverse}>Reverse</button>
+      </div>
       <div className="sky"></div>
       <div className="earth">
-        <div id="red-queen_and_alice">
-          <img id="red-queen_and_alice_sprite" ref={alice} src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png" alt="Alice and the Red Queen running to stay in place." />
+        <img id="mountain" src="/images/mountain.png" alt="Ground" />
+        <div id="cube">
+          <img id="cube_sprite" ref={cube} src="/images/spritesheet.png" alt="Cube running" />
         </div>
       </div>
-      <div class="scenery" id="foreground1">
-        <img id="palm3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png" alt=" " />
+      <div className="scenery" ref={foreground1} id="foreground1">
+        <img id="tree1" src="/images/tree-1.png" alt="Tree" />
       </div>
-      <div class="scenery" id="foreground2">
-        <img id="bush" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png" alt=" " />
-        <img id="w_rook_upright" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png" alt=" " />
+      <div className="scenery" ref={foreground2} id="foreground2">
+        <img id="bush" src="/images/bush.png" alt="Bush" />
+        <img id="rock_upright" src="/images/rock-1.png" alt="Rock" />
       </div>
-      <div class="scenery" id="background1">
-        <img id="r_pawn_upright" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" alt=" " />
-        <img id="w_rook" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png" alt=" " />
-        <img id="palm1" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png" alt=" " />
+      <div className="scenery" ref={background1} id="background1">
+        <img id="rock2_upright" src="/images/rock-2.png" alt="Rock" />
+        <img id="rocks" src="/images/rock-3.png" alt="Rock" />
+        <img id="tree2" src="/images/tree-2.png" alt="Tree" />
       </div>
-      <div class="scenery" id="background2">
-        <img id="r_pawn" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png" alt=" " />
-
-        <img id="r_knight" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png" alt=" " />
-        <img id="palm2" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png" alt=" " />
+      <div className="scenery" ref={background2} id="background2">
+        <img id="rock" src="/images/rock-4.png" alt="Rock" />
+        <img id="tree3" src="/images/tree-3.png" alt="Tree" />
       </div>
     </div>
   );
